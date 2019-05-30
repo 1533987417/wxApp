66c729f4e67087d725cb3ae3d4a01c5822aeacd8
author <1533987417@qq.com>
2019-05-30 19:20:07 +0800

    首页样式变更

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@
 //var host = 'https://www.easywork.net.cn/kjc';
 //var host = 'http://192.168.1.102:8082';
 //var host = 'http://localhost:8080'
-var host = 'https://www.wangyao.online/proseer';
+var host = 'https://www.proseer.cn/zcxy';
 var api = {
 
   // 下面的地址配合云端 Demo 工作
